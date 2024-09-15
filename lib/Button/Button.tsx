8765cf41ed4b5766d95a65c551e08ac82e692c6a
Saif Al-Din Ali <thesaifaldin@gmail.com>
2024-09-15 18:44:19 -0400

    Make button

---
+++ lib/Button/Button.tsx
@@ -0,0 +1,10 @@
+import type { ReactNode } from "react";
+import React from "react";
+
+interface ButtonProps {
+  children: ReactNode;
+}
+
+export const Button = ({ children }: ButtonProps): ReactNode => {
+  return <button>{children}</button>;
+};

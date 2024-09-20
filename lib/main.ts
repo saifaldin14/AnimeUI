259cf795af190e8bd58142f19c3268f1e3aee870
Saif Al-Din Ali <thesaifaldin@gmail.com>
2024-09-19 20:17:55 -0400

    Update main.ts

--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,4 @@
 import './tailwind.css'
 export { Button } from './Buttons/Button/Button'
 export { SpeechBubbleButton } from './Buttons/SpeechBubbleButton/SpeechBubbleButton'
+export { Accordion } from './Disclosure/Accordion/Accordion'

cee8ce83faf62f70ddc5dc28f7e3dd1d56d5dd3f
Saif Al-Din Ali <thesaifaldin@gmail.com>
2024-09-21 15:35:23 -0400

    Modal component

--- lib/Overlay/Modal/Modal.stories.tsx
+++ lib/Overlay/Modal/Modal.stories.tsx
@@ -1,19 +1,19 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Drawer } from "./Drawer";
+import { Modal } from "./Modal";
 
-const meta: Meta<typeof Drawer> = {
-  component: Drawer,
+const meta: Meta<typeof Modal> = {
+  component: Modal,
 };
 
 export default meta;
 
-type Story = StoryObj<typeof Drawer>;
+type Story = StoryObj<typeof Modal>;
 
 export const Default: Story = {
   args: {
     isOpen: true,
-    children: "Hey this is a Drawer!",
+    children: "Hey this is a Modal!",
     onClose: () => {},
   },
 };
@@ -21,11 +21,10 @@ export const Default: Story = {
 export const CustomColours: Story = {
   args: {
     isOpen: true,
-    children: "Hey this is a Drawer!",
+    children: "Hey this is a alert dialog!",
     onClose: () => {},
-    fromColor: "red",
+    fromColor: "pink",
     toColor: "lightblue",
-    elementColor: "black",
     textColor: "black",
   },
 };
